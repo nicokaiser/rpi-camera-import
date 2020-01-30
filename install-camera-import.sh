253317e80cfe74c6937ab0481ca6d8ace45b5bb3
Nico Kaiser <nico@kaiser.me>
2020-01-30 11:59:33 +0100

    fix: Avoid prompt about usbmount.conf

--- install-camera-import.sh
+++ install-camera-import.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 apt install -y --no-install-recommends python3-gphoto2 python3-blinkt python3-psutil lockfile-progs exfat-fuse
+dpkg -i /usr/local/src/usbmount_0.0.24_all.deb
 
 cp -a rootfs/* /
-
-dpkg -i /usr/local/src/usbmount_0.0.24_all.deb
 
 systemctl daemon-reload
 systemctl enable blinkt-clear.service
