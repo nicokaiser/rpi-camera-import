7a42ce4253fc72f0abce531a41e3197c560184da
Nico Kaiser <nico@kaiser.me>
2020-01-31 12:33:28 +0100

    fix: Do not prompt for usbmount config on installation

--- install-camera-import.sh
+++ install-camera-import.sh
@@ -4,7 +4,7 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 apt install -y --no-install-recommends python3-gphoto2 python3-blinkt python3-psutil lockfile-progs exfat-fuse
 cp -a rootfs/* /
-DEBIAN_FRONTEND=noninteractive dpkg -i /usr/local/src/usbmount_0.0.24_all.deb
+dpkg -i --force-confold /usr/local/src/usbmount_0.0.24_all.deb
 
 systemctl daemon-reload
 systemctl enable blinkt-clear.service
