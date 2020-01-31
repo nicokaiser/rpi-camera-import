c6d1553958cd7d9ede6c518f12f4e92ddc448a1a
Nico Kaiser <nico@kaiser.me>
2020-01-31 12:28:27 +0100

    fix: Fix usbmount installation

--- install-camera-import.sh
+++ install-camera-import.sh
@@ -3,9 +3,8 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 apt install -y --no-install-recommends python3-gphoto2 python3-blinkt python3-psutil lockfile-progs exfat-fuse
-dpkg -i /usr/local/src/usbmount_0.0.24_all.deb
-
 cp -a rootfs/* /
+DEBIAN_FRONTEND=noninteractive dpkg -i /usr/local/src/usbmount_0.0.24_all.deb
 
 systemctl daemon-reload
 systemctl enable blinkt-clear.service
